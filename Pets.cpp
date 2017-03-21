2c9775210daa9328546809fb7163a3ffe4752831
Arles96 <arles.cerrato@gmail.com>
2017-03-21 06:45:09 -0600

    correction in pets class

--- Pets.cpp
+++ Pets.cpp
@@ -11,14 +11,6 @@ Pets::Pets(string name)
 
 Pets::~Pets()
 {
-  for (int i=0; i < foods.size(); i++){
-    delete foods[i];
-  }
-  food.clear();
-  for (int i=0; i<toys.size(); i++){
-    delete toys[i];
-  }
-  toys.clear();
 }
 
 string Pets::getName()
@@ -49,44 +41,4 @@ void Pets::setStomach(int stomach)
 void Pets::setFeeling(int feeling)
 {
   this->feeling=feeling;
-}
-
-void Pets::addToy(Toys* t)
-{
-  toys.push_back(t);
-}
-
-void addFood(Food* f)
-{
-  foods.push_back(f);
-}
-
-int Pets::sizeToys()
-{
-  return toys.size();
-}
-
-int Pets::sizeFoods()
-{
-  return foods.size();
-}
-
-Toys* Pets::getToy(int i)
-{
-  return toys[i];
-}
-
-Food* Pets::getFood(int i)
-{
-  return foods[i];
-}
-
-void Pets::removeToy(int i)
-{
-  toys.erase(toys.begin()+i);
-}
-
-void Pets::removeFood(int i)
-{
-  foods.erase(foods.begin()+i);
 }
--- Pets.h
+++ Pets.h
@@ -14,8 +14,6 @@ private:
   string name;
   int stomach;
   int feeling;
-  vector <Toys*> toys;
-  vector <Food*> foods;
 
 public:
 
@@ -36,26 +34,12 @@ public:
   void setStomach(int);
   void setFeeling(int);
 
-  //Funciones para la admisnitracion de los vectores
-  //Agregar a los vectores
-  void addToy(Toys*);
-  void addFood(Food*);
-  //retorna el tamaño del vector
-  int sizeToys();
-  int sizeFoods();
-  //Retorna un articulo del vector
-  Toys* getToy(int);
-  Food* getFood(int);
-  //Funcion de eliminar articulos en los vectores
-  void removeToy(int);
-  void removeFood(int);
-
   //Funciones virtuales
   virtual void play()=0;
   virtual void giveMoney()=0;
   virtual void eat()=0;
   virtual void move()=0;
-  virtual void restriction()=0;
+  
  };
 
 
